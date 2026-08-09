a2318512332bfac602f648f38849216e576dedd0
Sonder <136065585+S0nder9@users.noreply.github.com>
2026-08-09 15:40:50 +0400

    chore: export project system from index

--- compiler/src/index.ts
+++ compiler/src/index.ts
@@ -13,3 +13,9 @@ export { CssGenerator } from "./codegen/css/CssGenerator";
 export { JsGenerator } from "./codegen/js/JsGenerator";
 
 export { BuildGenerator } from "./codegen/BuildGenerator";
+
+export type { EasySConfig } from "./config/EasySConfig";
+export { ConfigParser } from "./config/ConfigParser";
+export { Project } from "./project/Project";
+export { findProject, tryFindProject } from "./project/findProject";
+export { SourceResolver } from "./project/SourceResolver";
